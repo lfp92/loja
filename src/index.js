20c9d1453143a4b35ea236adc7662fcf630f7398
Leonardo F Petrauskas <leonardopetrauskas@hotmail.com>
2020-11-23 22:04:49 -0300

    alterado posicao dos itens no index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import store from "./store";
-import ProductDetail from "./components/ProductDetail/ProductDetail";
-import App from "./components/App/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+import App from './components/App/App';
 import './reset.css';
-import Products from "./components/Products/Products";
+import Products from './components/Products/Products';
+import Cart from './components/Cart/Cart';
 
 ReactDOM.render(
   <div>
     <Provider store={store}>
       <Router>
-        <App>
-          <Switch>
-            <Route exact path="/" component={Products}>
-            </Route>
-            <Route path="/produto/:id" component={ProductDetail} />
-          </Switch>
-        </App>
+        <Switch>
+          <Route exact path="/" component={App}></Route>
+          <Route path="/produto/:id" component={ProductDetail} />
+          <App />
+        </Switch>
       </Router>
     </Provider>
   </div>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
