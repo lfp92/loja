c0ee9a3c4fca472e9774999c00c8c2b9c758ae69
Leonardo F Petrauskas <leonardopetrauskas@hotmail.com>
2020-11-24 19:12:02 -0300

    Item transformado em function

--- src/components/Products/Item.jsx
+++ src/components/Products/Item.jsx
@@ -1,33 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import AddButton from "../Buttons/AddButton";
-import styled from "styled-components";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AddButton from '../Buttons/AddButton';
+import styled from 'styled-components';
 
-class Item extends React.Component {
-
-  render() {
-    return (
-      <Div>
-        <Link to={`/produto/${this.props.product.id}`}>
-          <H2Link>
-            {this.props.product.name}
-          </H2Link>
-        </Link>
-        <DivAux>
-          <h3>Código: {this.props.product.id}</h3>
-          <h4>R$ {this.props.product.price}</h4>
-          <section>
-            Descrição: {this.props.product.description.substr(0, 50)}...
+function Item(props) {
+  return (
+    <Div>
+      <Link to={`/produto/${props.product.id}`}>
+        <H2Link>{props.product.name}</H2Link>
+      </Link>
+      <DivAux>
+        <h3>Código: {props.product.id}</h3>
+        <h4>R$ {props.product.price}</h4>
+        <section>
+          Descrição: {props.product.description.substr(0, 50)}...
         </section>
-          <DivCenter>
-            <AddButton product={this.props.product}>Adicionar ao carrinho</AddButton>
-          </DivCenter>
-        </DivAux>
-
-      </Div>
-    );
-  }
+        <DivCenter>
+          <AddButton product={props.product}>Adicionar ao carrinho</AddButton>
+        </DivCenter>
+      </DivAux>
+    </Div>
+  );
 }
 
 export default Item;
@@ -38,19 +31,19 @@ const H2Link = styled.h2`
   font-size: 1.2rem;
   margin-top: 0px;
   padding: 10px;
-`
+`;
 const Div = styled.div`
   background-color: #fefefe;
   border: 1px solid black;
   border-radius: 2px;
   margin: 10px;
-`
+`;
 const DivAux = styled.div`
   color: #222222;
-  padding: 0px 10px 10px 10px; 
-`
+  padding: 0px 10px 10px 10px;
+`;
 const DivCenter = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 10px;
-` 
+`;
